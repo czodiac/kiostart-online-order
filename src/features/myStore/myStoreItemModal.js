536f52cf410c74c2c83c88fcc88658e25ac79980
ITL Studio <iltae80@daum.net>
2022-11-10 21:34:47 -0700

    Remove ugly black border

--- src/features/myStore/myStoreItemModal.js
+++ src/features/myStore/myStoreItemModal.js
@@ -15,9 +15,10 @@ const style = {
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    borderRadius: '3px',
     boxShadow: 24,
     p: 4,
+    outline: 'none',
 };
 
 export const MyStoreItemModal = () => {
@@ -33,7 +34,6 @@ export const MyStoreItemModal = () => {
 
     return (
         <div>
-            <Button onClick={handleOpen}>Open modal</Button>
             <Modal
                 keepMounted
                 open={open}
